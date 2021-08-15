251df2b8676c434d1132114e88e417f53eeaa121
Adam Mertzenich <adam@mertzeni.ch>
2021-08-15 18:20:40 -0500

    VS-Code download tools

--- download-tools.bat
+++ download-tools.bat
@@ -1,3 +1,4 @@
 @echo off
 start https://github.com/git-for-windows/git/releases/tag/v2.33.0-rc2.windows.1
 start https://github.com/gohugoio/hugo/releases/tag/v0.87.0
+start https://code.visualstudio.com/download
